b7be96ae6878f04ca0abe52f27314487d75acbeb
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2020-05-27 17:55:04 -0300

    VB

--- Natura/Form2.vb
+++ Natura/Form2.vb
@@ -294,6 +294,6 @@ Public Class Form2
     End Sub
 
     Private Sub ToolStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles ToolStrip1.ItemClicked
-        ' jhj
+
     End Sub
 End Class
