829f3486f315d3c60a6de43bd5b34988bf7c23d5
Paulo Bruno <53666610+PauloCesarBruno@users.noreply.github.com>
2021-04-04 20:30:55 -0300

    Acerto String OLEDB

--- Natura/Module1.vb
+++ Natura/Module1.vb
@@ -2,7 +2,7 @@
 Imports System.Data.OleDb
 Module Module1
     Public Function GetConnection() As OleDbConnection
-        Dim sql As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Paulo Bruno\sql\Cópia Banco de Dados\BancoNatura.mdb"
+        Dim sql As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\Conteudo do meu C\Paulo Bruno\sql\Cópia Banco de Dados\BancoNatura.mdb"
         Return New OleDbConnection(sql)
     End Function
 
--- Natura/My Project/Settings.Designer.vb
+++ Natura/My Project/Settings.Designer.vb
@@ -15,21 +15,21 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
         
         Private Shared defaultInstance As MySettings = CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New MySettings()),MySettings)
         
-#Region "Funcionalidade de Salvamento Automático de Minhas Configurações"
+#Region "Funcionalidade de salvamento automático do My.Settings"
 #If _MyType = "WindowsForms" Then
     Private Shared addedHandler As Boolean
 
     Private Shared addedHandlerLockObject As New Object
 
     <Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)> _
-    Private Shared Sub AutoSaveSettings(ByVal sender As Global.System.Object, ByVal e As Global.System.EventArgs)
+    Private Shared Sub AutoSaveSettings(sender As Global.System.Object, e As Global.System.EventArgs)
         If My.Application.SaveMySettingsOnExit Then
             My.Settings.Save()
         End If
@@ -57,8 +57,8 @@ Namespace My
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Paulo Bruno\sql\Cópia Banco de Da"& _ 
-            "dos\BancoNatura.mdb")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=""D:\Conteudo do meu C\Paulo Bruno\sq"& _ 
+            "l\Cópia Banco de Dados\BancoNatura.mdb""")>  _
         Public ReadOnly Property BancoNaturaConnectionString() As String
             Get
                 Return CType(Me("BancoNaturaConnectionString"),String)
